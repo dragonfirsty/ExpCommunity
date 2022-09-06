6f935b9526776d6a48d6c5ed033c2cf267e71c87
leonardo moya fernandes <leonardomoya03@gmail.com>
2022-09-06 16:47:11 -0300

    Feat: model and serializer comment

---
+++ comments/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+from .models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = "__all__"
+        extra_kwargs = {
+            "created_at": {"read_only": True},
+            "id": {"read_only": True},
+            "updated_at": {"read_only": True},
+        }
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    post = PostSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ["description", "user", "post"]
