5b7f44bb593761c36dbbf5ba79e704c6de1c7350
leonardo moya fernandes <leonardomoya03@gmail.com>
2022-09-13 17:07:54 -0300

    Fix: post app all routes

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("posts/<str:group_id>/", views.PostView.as_view()),
-    path("posts/<pk>/", views.PostDetailView.as_view()),
+    path("posts/detail/<pk>/", views.PostDetailView.as_view()),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import get_object_or_404
+from groups.models import Group
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+
 from .models import Post
-from groups.models import Group
-from .serializers import PostSerializer
 from .permissions import AbleToPost, IsOwner
+from .serializers import PostSerializer
+
 
 class PostView(generics.ListCreateAPIView):
     authentication_classes = [TokenAuthentication]
@@ -13,16 +15,18 @@ class PostView(generics.ListCreateAPIView):
 
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    lookup_field = 'group_id'
+    lookup_field = "group_id"
+
     def perform_create(self, serializer):
-        
-        serializer.save(user_id = self.request.user)
-    
+
+        group_id = get_object_or_404(Group, pk=self.kwargs.get("group_id"))
+
+        serializer.save(user=self.request.user, group=group_id)
 
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated] 
+    permission_classes = [IsAuthenticated]
 
     queryset = Post.objects.all()
     serializer_class = PostSerializer
