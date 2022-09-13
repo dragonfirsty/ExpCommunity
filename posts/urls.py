db55d45a38d036aecece8382305d91721eb1579d
leonardo moya fernandes <leonardomoya03@gmail.com>
2022-09-13 13:51:49 -0300

    Fix:  post app

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("posts/<int:group_id>/", views.PostView.as_view()),
+    path("posts/<str:group_id>/", views.PostView.as_view()),
     path("posts/<pk>/", views.PostDetailView.as_view()),
 ]
